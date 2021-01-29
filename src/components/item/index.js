c2a1ee068c7545fd137562fa572b4eb95cd1c61c
Daniela Córdoba <dacorac43@gmail.com>
2021-01-28 21:25:02 -0500

    Adición de estilos al componente Item

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './item.css'
+
 const Item = ({id, title, shipping, condition, price, url}) => {
     return ( 
-        <Link to={`/items/${id}`}>
+        <Link className="link" to={`/items/${id}`}>
             <div className="card">
                 <div className="card-image margin-16">
                     <img alt={title} src={url} />
                 </div>
-                <div className="margin-16">
+                <div className="card-content margin-16">
                     <div className="price-box padding-32">
                         <h1 className="font-24 ft-color-black">$ {price}</h1>
                         { shipping ? <img alt="shipping" src="/icon/shipping@2x.png"/> : null }
