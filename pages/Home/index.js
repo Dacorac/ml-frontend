008bd91925f8be4e8d67c32730bfb07273475881
Daniela Córdoba <dacorac43@gmail.com>
2021-01-28 21:24:40 -0500

    Adición de estilos al componente Home

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,24 +1,11 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
+import './home.css'
+
 const HomePage = () => {
-
-    const [term, setTerm] = useState('');
-
-    const handleInputChange = (e) => setTerm(e.target.value);
-
-    let history = useHistory();
-
-    const handleButtonClick = (e) => {
-        e.preventDefault();
-        
-        history.push(`/items?search=${term}`);
-    }
-
     return (
         <div>
-            <input type="text" onChange={handleInputChange} value={term} />
-            <button onClick={handleButtonClick}>click</button>
         </div>
       );
 }
