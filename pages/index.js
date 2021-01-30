d57489e797b68ee7bcb84cb2fb4ccc412d042c4a
Daniela Córdoba <dacorac43@gmail.com>
2021-01-30 17:10:20 -0500

    Adición de los metatags para aplicación SEO Friendly

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,14 @@ import Search from '../src/components/search'
 export default function Home() {
   return (
     <Fragment>
+      <Head>
+        <title>Mercado Libre | Prueba técnica</title>
+        <meta property="og:type" content="company" />
+        <meta property="og:title" content="Mercado Libre | Prueba técnica" />
+        <meta property="og:description" content="Aplicación desarrollada utilizando NextJS para prueba técnica MercadoLibre." />
+        <meta name="keywords" content="ReactJS, NextJS, HTML, CSS, JavaScript" />
+        <meta name="author" content="Daniela Córdoba" />
+      </Head>
       <Store>
         <Search />
         <Routes />
