1bdfc6b9efdbc4350e44dedca8824d42b6ead8e4
Daniela Córdoba <dacorac43@gmail.com>
2021-01-30 20:08:49 -0500

    Remover console.log del proyecto

--- src/hoc/withLoading.js
+++ src/hoc/withLoading.js
@@ -1,6 +1,5 @@
 function withLoading(Component) {
     return function withLoadingComponent({ isLoading, ...props }) {
-        console.log("isLoading", isLoading);
         if (!isLoading) return <Component {...props} />;
         return <span>Loading...</span>;
     };
--- src/hooks/useItems.js
+++ src/hooks/useItems.js
@@ -2,7 +2,6 @@ import { useQuery } from "react-query"
 import fetchItems from '../services/items.service';
 
 const useItems = (query) => {
-    console.log(query)
     return useQuery([ query ], fetchItems);
 };
 
